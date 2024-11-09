d019860c185d1aa52ae99975a7b8369125d0f0e1
Jonas Engström <j.e.engstrom@gmail.com>
2024-11-09 17:35:19 +0100

    Added function to download CPI. Fixes #7

---
+++ R/get_cpi.R
@@ -0,0 +1,49 @@
+#' Get Consumer Price Index from Statistics Sweden
+#'
+#' Downloads consumer price index from [the Statistics Sweden API](https://www.statistikdatabasen.scb.se).
+#'
+#' @return A tibble of historical consumer price indes values.
+#' @export
+#'
+#' @md
+#' @examples
+#' \dontrun{
+#' cpi_data <- get_cpi()
+#' }
+get_cpi <- function() {
+  NULL ->
+    key ->
+    values
+
+  'https://api.scb.se' |>
+    httr2::request() |>
+    httr2::req_url_path(
+      'OV0104/v1/doris/sv/ssd/START/PR/PR0101/PR0101A/KPItotM'
+    ) |>
+    httr2::req_body_json(
+      list(
+        query = list(
+          list(
+            code = "ContentsCode",
+            selection = list(
+              filter = "item",
+              values = list("000004VU")
+            )
+          )
+        ),
+        response = list(
+          format = "json"
+        )
+      )
+    ) |>
+    httr2::req_perform() |>
+    httr2::resp_body_json(simplifyVector = TRUE) |>
+    purrr::pluck('data') |>
+    tidyr::unnest(cols = dplyr::everything()) |>
+    dplyr::mutate(
+      key = stringr::str_replace(key, '(\\d{4})M(\\d{2})', '\\1-\\2-01') |> as.Date(),
+      values = as.numeric(values)
+    ) |>
+    dplyr::rename(date = key, cpi = values) |>
+    (\(x) return(x))()
+}
