b1ada2098d280c0ad6d71365607676d4d39b573f
Jonas Engström <j.e.engstrom@gmail.com>
2024-11-10 13:50:33 +0100

    Added function to calculate debt ratio.

---
+++ R/debt_ratio.R
@@ -0,0 +1,16 @@
+#' Calculate Debt Ratio
+#'
+#' Calculate debt ratio as defined by [Finansinspektionen](https://www.fi.se/contentassets/690c9d8430bd4961b6073dedf4f9105f/fs1616-k2024.pdf).
+#'
+#' @param remaining_principal Remaining principal.
+#' @param household_gross_income Household gross income.
+#'
+#' @return A quotient.
+#' @export
+#'
+#' @md
+#' @examples
+#' debt_ratio(400, 100)
+debt_ratio <- function(remaining_principal, household_gross_income) {
+  return(remaining_principal / household_gross_income)
+}
---
+++ tests/testthat/test-debt_ratio.R
@@ -0,0 +1,3 @@
+test_that("debt_ratio returns expected value", {
+  expect_equal(debt_ratio(400, 100), 4)
+})
